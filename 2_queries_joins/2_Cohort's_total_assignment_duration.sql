b723a3c33b98d1552d1b7eb57f61cee39ba80ce0
Jacky Ngo <jacky.ngo96@gmail.com>
2023-01-16 11:33:02 -0500

    complete 'Group-by' challenge

--- 2_queries_joins/2_Cohort's_total_assignment_duration.sql
+++ 2_queries_joins/2_Cohort's_total_assignment_duration.sql
@@ -2,4 +2,11 @@ SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = 'FEB12';
+WHERE cohorts.name = 'FEB12'; 
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE end_date IS NULL
+GROUP BY students.name;
